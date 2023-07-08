74cb67813354367f6be3786911e6e1f2974260a4
kaarthik108 <kaarthikandavar@gmail.com>
2023-07-08 15:53:40 +1200

    Fix signin

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
 
-export const runtime = "edge";
-
 export default function Page() {
   return (
     <div className="flex items-center justify-center h-screen">
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
 import React from "react";
-export const runtime = "edge";
 
 export default function Page() {
   return <SignUp />;
