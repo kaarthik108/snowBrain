1ea23c7e8d7815e577274ce7ee5aa05e7af656cd
kaarthik108 <kaarthikandavar@gmail.com>
2023-07-10 09:14:38 +1200

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,15 +3,23 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const ip = req.ip ?? "127.0.0.1";
+  const ip = req.headers.get("x-forwarded-for") || "127:0:0:1";
 
   try {
     if (process.env.NODE_ENV != "development") {
-      const { success } = await rateLimiter.limit(ip);
+      const { success, limit, remaining, reset } = await rateLimiter.limit(ip);
 
       if (!success)
         return new NextResponse(
-          "There is a rate limit on this API. Please try again later."
+          "There is a rate limit on this API. Please try again later.",
+          {
+            status: 429,
+            headers: {
+              "X-RateLimit-Limit": limit.toString(),
+              "X-RateLimit-Remaining": remaining.toString(),
+              "X-RateLimit-Reset": reset.toString(),
+            },
+          }
         );
 
       if (req.nextUrl.pathname.startsWith("/api")) {
