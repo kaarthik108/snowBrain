e8d74b6bba8ac180ed5d8c0ded4a39b6d60941a8
kaarthik108 <kaarthikandavar@gmail.com>
2023-07-15 20:34:26 +1200

    Fix build on edge func

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,13 +1,31 @@
+import { type Metadata } from 'next'
 import { notFound, redirect } from 'next/navigation'
 
 import { getChat } from '@/app/actions'
 import { auth } from '@/auth'
 import { Chat } from '@/components/chat'
 
-export const runtime = 'edge'
+// export const runtime = 'edge'
+export const preferredRegion = 'home'
+
 export interface ChatPageProps {
   params: {
     id: string
+  }
+}
+
+export async function generateMetadata({
+  params
+}: ChatPageProps): Promise<Metadata> {
+  const session = await auth()
+
+  if (!session?.user) {
+    return {}
+  }
+
+  const chat = await getChat(params.id)
+  return {
+    title: chat?.title.toString().slice(0, 50) ?? 'Chat'
   }
 }
 
