771575093b0427a47a6529976861c95a6115d8cc
kaarthik108 <kaarthikandavar@gmail.com>
2023-07-15 20:47:53 +1200

    remove edge

--- app/share/[id]/page.tsx
+++ app/share/[id]/page.tsx
@@ -1,12 +1,12 @@
 import { type Metadata } from 'next'
 import { notFound } from 'next/navigation'
 
-import { formatDate } from '@/lib/utils'
 import { getSharedChat } from '@/app/actions'
 import { ChatList } from '@/components/chat-list'
 import { FooterText } from '@/components/footer'
+import { formatDate } from '@/lib/utils'
 
-export const runtime = 'edge'
+// export const runtime = 'edge'
 export const preferredRegion = 'home'
 
 interface SharePageProps {
